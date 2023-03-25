8374bf825732a1cca9c235b95c7f993cd815bc33
Santiago Villordo <santiago.villordo@gmail.com>
2023-03-25 16:44:28 -0300

    adicion de estilos mejorando html

--- app/app.js
+++ app/app.js
@@ -1,16 +1,16 @@
+//guardar las direcciones de las apis en variables
 const API_BASE = "https://rickandmortyapi.com/api";
 const API_CHARACTERS = "https://rickandmortyapi.com/api/character";
 
-// consumir la api con JS metodo peticion get metodo fetch
-//paso 1 guardar la llamada de la api en una variable
+//guardar la llamada de la api en una variable
 const llamadaApi = fetch(API_CHARACTERS);
 
-//paso 2 esperar la respuesta de la api y convertirla a formato json
+//esperar la respuesta de la api y convertirla a formato json
 llamadaApi
   .then((data) => {
     return data.json();
   })
-  //paso 3 en el ultimo then la data llega formateada lista para usar
+  //en el ultimo then la data llega formateada lista para usar
   .then((data) => {
     const $container = document.getElementById("container");
     const characters = data.results;
@@ -32,7 +32,7 @@ llamadaApi
         `;
     }
   })
-  //paso 4 en caso de error aparece en el catch
+  //en caso de error aparece en el catch
   .catch((error) => {
     console.log(error);
   });
